625c93ff663f084ec9fa184e9b7f3a2079201012
helenplesko <146447100+helenplesko@users.noreply.github.com>
2024-05-23 10:19:02 -0400

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,28 @@
+// Function to make navigation bar responsive, change to hamburger when screen size reduces
+function responsiveNav(y) {
+  y.classList.toggle("change");
+  let x = document.getElementById("myNav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+// Function for back to top button
+// Get the button
+let mybutton = document.getElementById("backtotop");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
